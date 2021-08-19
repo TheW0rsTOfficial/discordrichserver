99c517f32642e6512f2bb5bd8ae6b003a77baa36
TheW0rsTOfficial <88917155+TheW0rsTOfficial@users.noreply.github.com>
2021-08-19 20:20:45 +0200

    Add files via upload

---
+++ twc.lua
@@ -0,0 +1,17 @@
+TheWorst = {}
+
+TheWorst.Discord = {
+    Wait = 15000, --DON'T TOUCH THIS
+    CheckWait = 10000, --DON'T TOUCH THIS
+    SlotFivem = "64", --SLOT FIVEM SERVER 
+    IdBot = 4530530450, -- ID BOT
+    LAssetImage = 'imagelarge',
+    LAssetText = 'Text Large Image',
+    SAssetImage = 'imagesmall',
+    SAssetText = 'Text Small Image',
+    ButtonActive = true, --ACTIVE BUTTON [TRUE/FALSE]
+    ActionButtonName1 = "Discord", -- Name 1 button
+    ActionDescriptionName1 = "Discord.gg/discord", -- Description 1 button
+    ActionButtonName2 = "Fivem", -- Name 2 button
+    ActionDescriptionName2 = "Link Fivem", -- Description 2 button
+}
